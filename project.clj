addd2a0f768d754dea95098791efff704b255739
Eli Naeher <eli@naeher.name>
2015-03-01 19:57:04 -0600

    Add :scm to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject enaeher/contrail "0.1.0-SNAPSHOT"
   :description "An interactive tracing facility for Clojure"
   :url "http://github.com/enaeher/contrail"
+  :scm {:name "git"
+        :url "http://github.com/enaeher/contrail"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
