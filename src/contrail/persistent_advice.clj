a6196802fa1c313c94ac855d38719f2bfbd7bb56
Eli Naeher <eli@naeher.name>
2015-03-11 21:04:26 -0500

    Refs #5 - Use metadata, not atom, to keep track of advising fns

--- src/contrail/persistent_advice.clj
+++ src/contrail/persistent_advice.clj
@@ -1,10 +1,10 @@
 (ns contrail.persistent-advice
   (:require [richelieu.core :as richelieu]))
 
-(defonce ^:private advised-vars (atom '{}))
+(defonce ^:private advised-vars (atom #{}))
 
 (defn all-advised []
-  (keys @advised-vars))
+  @advised-vars)
 
 (defn current-advised []
   richelieu/*current-advised*)
@@ -14,22 +14,27 @@
   has been re-defined by the user"
   false)
 
+(defn get-our-advising-fn [f]
+  (first (filter (comp :traced-by-contrail meta) (richelieu/advice (deref f)))))
+
 (defn unadvise [f]
   (binding [*suppress-readvising?* true]
     (remove-watch f "contrail")
-    (richelieu/unadvise-var f (@advised-vars f))
-    (swap! advised-vars dissoc f)))
+    (richelieu/unadvise-var f (get-our-advising-fn f))
+    (swap! advised-vars disj f)))
 
 (defn advise* [f advice-fn]
   (richelieu/advise-var f advice-fn)
-  (swap! advised-vars assoc f advice-fn))
+  (swap! advised-vars conj f))
 
 (defn advise [f wrapping-fn]
-  (advise* f (wrapping-fn f))
-  (add-watch f "contrail"
-             (fn [_ advised-var _ new-var-value]
-               (when-not *suppress-readvising?*
-                 (binding [*suppress-readvising?* true]
-                   (println f "changed, re-tracing.")
-                   (advise* advised-var (wrapping-fn (richelieu/advised new-var-value))))))))
+  (letfn [(meta-wrapping-fn [f]
+            (with-meta (wrapping-fn f) {:traced-by-contrail true}))]
+    (advise* f (meta-wrapping-fn f))
+    (add-watch f "contrail"
+               (fn [_ advised-var _ new-var-value]
+                 (when-not *suppress-readvising?*
+                   (binding [*suppress-readvising?* true]
+                     (println f "changed, re-tracing.")
+                     (advise* advised-var (meta-wrapping-fn (richelieu/advised new-var-value)))))))))
 
