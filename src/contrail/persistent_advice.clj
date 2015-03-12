d5422e98bed88bf47e861713141a611e346b5ad4
Eli Naeher <eli@naeher.name>
2015-03-11 21:08:05 -0500

    Refs #5 - Cleanup in persistent-advice

--- src/contrail/persistent_advice.clj
+++ src/contrail/persistent_advice.clj
@@ -14,7 +14,13 @@
   has been re-defined by the user"
   false)
 
-(defn get-our-advising-fn [f]
+(defn get-our-advising-fn
+  "Of the various Richelieu advising functions which may advice `f`, a
+  var, returns the first (and hopefully only) one created by
+  Contrail."
+  [f]
+  ;; The deref is necessary because Richeleu actually sets the advice
+  ;; on the function, not the var
   (first (filter (comp :traced-by-contrail meta) (richelieu/advice (deref f)))))
 
 (defn unadvise [f]
@@ -27,14 +33,16 @@
   (richelieu/advise-var f advice-fn)
   (swap! advised-vars conj f))
 
+(defn get-watcher [wrapping-fn]
+  (fn [_ advised-var _ new-var-value]
+    (when-not *suppress-readvising?*
+      (binding [*suppress-readvising?* true]
+        (println advised-var "changed, re-tracing.")
+        (advise* advised-var (wrapping-fn (richelieu/advised new-var-value)))))))
+
 (defn advise [f wrapping-fn]
   (letfn [(meta-wrapping-fn [f]
             (with-meta (wrapping-fn f) {:traced-by-contrail true}))]
     (advise* f (meta-wrapping-fn f))
-    (add-watch f "contrail"
-               (fn [_ advised-var _ new-var-value]
-                 (when-not *suppress-readvising?*
-                   (binding [*suppress-readvising?* true]
-                     (println f "changed, re-tracing.")
-                     (advise* advised-var (meta-wrapping-fn (richelieu/advised new-var-value)))))))))
+    (add-watch f "contrail" (get-watcher meta-wrapping-fn))))
 
